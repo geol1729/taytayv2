964598882297de4e82177b8606d1914c20cdb94d
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-08-12 11:48:30 -0700

    Normalize the emotions in each song

--- parseLyrics.js
+++ parseLyrics.js
@@ -9,42 +9,66 @@ const _ = require('lodash');
 let lyrics = fs.readFileSync('./lyrics.json', {encoding: 'utf-8'});
 lyrics = JSON.parse(lyrics);
 // load in emotion lexicon
-let emotions = fs.readFileSync('./emotion-lexicon.json', {encoding: 'utf-8'})
-emotions = JSON.parse(emotions);
+let emotionsLex = fs.readFileSync('./emotion-lexicon.json', {encoding: 'utf-8'})
+emotionsLex = JSON.parse(emotionsLex);
 
 const songs = _.chain(lyrics)
   .map(song => {
     if (!song.album) return;
+
+    let totalCount = 0;
     const lyrics = _.chain(song.lyrics.split('\n'))
       .map(line => {
         let words = line.match(/(\w+)/g);
         if (!words) return;
+
+        // remember number of words in the line
+        const count = words.length;
+        // add that count to the total num of words in the song
+        totalCount += count;
+
         // go through array of words, and remember emotions
+        // filter out words that didn't match emotions
         words = _.chain(words)
           .map(word => {
-            if (!emotions[word]) return;
-            return {word, emotions: emotions[word]};
+            if (!emotionsLex[word]) return;
+            return {word, emotions: emotionsLex[word]};
           }).filter().value();
 
+        const emotions = _.chain(words).map('emotions').flatten().countBy().value();
+        const normalized = _.reduce(emotions, (obj, numWords, emotion) => {
+          // absolute number of this emotion divided by total num words in the line
+          obj[emotion] = numWords / count;
+          return obj;
+        }, {})
+
         return {
-          line, words,
-          emotions: _.chain(words).map('emotions').flatten().countBy().value(),
+          line, words, count,
+          emotions, normalized,
         }
       }).filter().value();
 
+    const emotions = _.chain(lyrics).map('emotions')
+      .reduce((obj, emotions) => {
+        // object of emotions, key: emotion, val: sum across the lines
+        _.each(emotions, (val, emotion) => {
+          if (!obj[emotion]) {
+            obj[emotion] = 0;
+          }
+          obj[emotion] += val;
+        });
+        return obj;
+      }, {}).value();
+
+    const normalized = _.reduce(emotions, (obj, numWords, emotion) => {
+      obj[emotion] = numWords / totalCount;
+      return obj;
+    }, {});
+
     return Object.assign(song, {
       lyrics,
-      emotions: _.chain(lyrics).map('emotions')
-        .reduce((obj, emotions) => {
-          // object of emotions, key: emotion, val: sum across the lines
-          _.each(emotions, (val, emotion) => {
-            if (!obj[emotion]) {
-              obj[emotion] = 0;
-            }
-            obj[emotion] += val;
-          });
-          return obj;
-        }, {}).value()
+      totalCount,
+      emotions, normalized,
     });
   }).filter().value();
 
